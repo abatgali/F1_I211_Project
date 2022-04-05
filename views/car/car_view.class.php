c903abe48c18b62aa3b5c66c856f9a4ce707fda8
pvarney13 <pvarney@iu.edu>
2022-04-05 15:11:45 -0400

    created car view

--- views/car/car_view.class.php
+++ views/car/car_view.class.php
@@ -6,7 +6,38 @@
  * Description: displays cars
  */
 
-class CarView extends View{
+class CarView extends View {
+    public function display($cars)
+    {
+        //display header
+        parent::header();
 
+        ?>
 
+        <table border="0">
+            <tr>
+                <th>ID</th>
+                <th>Chassis</th>
+                <th>Power Unit</th>
+                <th>Car Image</th>
+                <th>Team</th>
+            </tr>
+            <!-- create a new row for each car -->
+            <?php
+            foreach ($cars as $car) {
+                echo "<tr>";
+                echo "<td>", $car->getCarID(), "</td>";
+                echo "<td>", $car->getChassis(), "</td>";
+                echo "<td>", $car->getPowerUnit(), "</td>";
+                echo "<td>", $car->getCarImage(), "</td>";
+                echo "<td>", $car->getTeam(), "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+        </body>
+        </html>
+
+        <?php
+    }
 }
