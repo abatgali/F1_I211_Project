bb09fad4416231484ff1424559a5bdc193080b50
abatgali <abatgali@iu.edu>
2022-04-25 13:57:08 -0400

    new fav button

--- views/driver/index/driver_index.class.php
+++ views/driver/index/driver_index.class.php
@@ -15,7 +15,6 @@ class DriverIndex extends DriverIndexView
         parent::displayHeader("F1 Drivers");
         ?>
 
-
             <div style="display: flex;" >
                 <button style="margin-top: 18px;" type="button" class="btn btn-warning mb-4" onclick="window.location.href='<?php echo BASE_URL."/driver/standings"; ?>'">Live Driver Standings</button>
 
@@ -51,14 +50,19 @@ class DriverIndex extends DriverIndexView
                 <a style="text-decoration: none; color: #3A1E1E" href="<?php echo BASE_URL."/driver/detail/".$driverID; ?>">
 
                 <img src="<?php echo BASE_URL."/static/img/drivers/".$driverID.".jpeg"; ?>" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $rNum;?></h5>
-                        <p class="card-text"><?php echo $lastName;?></p>
-                        <input id="favorites" type="checkbox" name="favorite">
-                        <label for="favorites" style="font-size: small">Favorite</label>
-                    </div>
-                </a>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $rNum;?></h5>
+                    <p class="card-text"><?php echo $lastName;?></p>
 
+                </a>
+                    <!--fav button-->
+                    <button onclick="window.location.href=''" type="button" class="btn btn-outline-danger float-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+                        </svg>
+                        Favorite
+                    </button>
+            </div>
             </div>
             <?php
             }
