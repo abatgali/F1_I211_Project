7e16d89a6c25cb07fd0f6230c851d98a35ace240
kavthakk <77465190+kavthakk@users.noreply.github.com>
2022-04-26 11:18:07 -0400

    Return home button!

--- views/user/logout/logout.class.php
+++ views/user/logout/logout.class.php
@@ -16,6 +16,8 @@ class Logout extends IndexView {
 
         <div class="container m-5">
             <h5>You have successfully logged out.</h5>
+            <br>
+            <button type="button" class="btn btn-primary mb-4 ms-2" onclick="window.location.href='<?php echo BASE_URL."/index"; ?>'">Return to home page</button>
         </div>
 
 
