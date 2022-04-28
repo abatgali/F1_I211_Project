95eb9406ad6285dcd9c78f9077b86e3b964bc687
abatgali <abatgali@iu.edu>
2022-04-27 20:43:18 -0400

    sticky footer for some pages

--- views/user/logout/logout.class.php
+++ views/user/logout/logout.class.php
@@ -28,9 +28,20 @@ class Logout extends IndexView {
             <button type="button" class="btn btn-primary mb-4 ms-2" onclick="window.location.href='<?php echo BASE_URL."/index"; ?>'">Return to home page</button>
         </div>
 
+        <footer class="d-block w-100 bg-dark py-3 position-absolute" style="bottom: 0">
+
+            <div class="container d-flex justify-content-between">
+                <p class="text-muted"> &copy; Group 3 - I211 Final Project</p>
+                <div class="text-muted">
+                    <p ><a href="https://www.formula1.com">Data Source: F1 Website</a></p>
+                    <p ><a href="https://www.postman.com/maintenance-astronomer-29796265/workspace/f1-api/collection/19328871-63c4a82c-ae84-4a24-a58b-bd8a408b1c4e?ctx=documentation">Standings: F1 Postman API </a></p>
+                    <p ><a href="https://racingnews365.com/formula-1-calendar-2022">Race Calendar Source</a></p>
+                </div>
+            </div>
+        </footer>
 
         <?php
-        parent::displayFooter();
+
     }
 
 }
--- views/user/profile/profile_view.class.php
+++ views/user/profile/profile_view.class.php
@@ -52,7 +52,19 @@ class ProfileView extends IndexView
 
         </div>
 
+        <!--sticky footer-->
+        <footer class="d-block w-100 bg-dark py-3 position-absolute" style="bottom: 0">
+
+            <div class="container d-flex justify-content-between">
+                <p class="text-muted"> &copy; Group 3 - I211 Final Project</p>
+                <div class="text-muted">
+                    <p ><a href="https://www.formula1.com">Data Source: F1 Website</a></p>
+                    <p ><a href="https://www.postman.com/maintenance-astronomer-29796265/workspace/f1-api/collection/19328871-63c4a82c-ae84-4a24-a58b-bd8a408b1c4e?ctx=documentation">Standings: F1 Postman API </a></p>
+                    <p ><a href="https://racingnews365.com/formula-1-calendar-2022">Race Calendar Source</a></p>
+                </div>
+            </div>
+        </footer>
 <?php
-        parent::displayFooter();
+
     }
 }
